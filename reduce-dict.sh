f1e5ebefb099fca22b5b4490ff3722bcd27111bb
Kevin Lyda <kevin@ie.suberic.net>
2012-03-31 12:41:21 +0100

    Style tweaks.
    Minor fixes to code style.

--- reduce-dict.sh
+++ reduce-dict.sh
@@ -8,5 +8,5 @@ fi
 find $1 -type f -print0 | xargs -0 cat \
   | tr ' \t' '\n\n' \
   | grep "^[a-zA-Z']*$" \
-  |sort -u >| all-words.$1
+  | sort -u > all-words.$1
 join misspellings.dict all-words.$1 > misspellings.$1.dict
