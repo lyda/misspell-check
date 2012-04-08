4ecd3a4505086dc9792dd77fdbc10f5e1696da20
Kevin Lyda <kevin@ie.suberic.net>
2012-04-08 13:03:55 +0100

    Checkpoint for tests.
    Framework for testing actually works.

--- tests/runall.py
+++ tests/runall.py
@@ -10,8 +10,8 @@ cwd = os.path.dirname(globals().get('__file__', os.getcwd()))
 
 test_list = [(os.path.basename(f)[:-3], f)
              for f in glob.glob(os.path.join(cwd, 'test_*.py'))]
-print test_list
 for test_module, test_file in test_list:
   test_runner = imp.load_source(test_module, test_file)
-  suite = unittest.defaultTestLoader.loadTestsFromModule(test_module)
+  print 'Running test suit "%s"' % test_module
+  suite = unittest.defaultTestLoader.loadTestsFromModule(test_runner)
   unittest.TextTestRunner(verbosity=2).run(suite)
--- tests/test_file_ops.py
+++ tests/test_file_ops.py
@@ -6,14 +6,13 @@ import unittest
 
 # Set the path to load the module being tested.
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-print sys.path
 import misspellings.misspellings_lib as misspellings
 
 
 class IntegerArithmenticTestCase(unittest.TestCase):
-  def testAdd(self):  ## test method names begin 'test*'
-    self.assertEqual((1 + 2), 3)
-    self.assertEqual(0 + 1, 1)
+  def testMissingList(self):
+    self.assertRaises(IOError, misspellings.Misspellings,
+                      None, 'missing_msl.txt')
   def testMultiply(self):
     self.assertEqual((0 * 10), 0)
     self.assertEqual((5 * 8), 40)
