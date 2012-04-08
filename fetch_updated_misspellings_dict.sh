c739c3c7a8e782c74a42d59ca9795a992e4b2173
Kevin Lyda <kevin@ie.suberic.net>
2012-04-08 19:02:47 +0100

    Add warning to edit the resulting list.

--- fetch_updated_misspellings_dict.sh
+++ fetch_updated_misspellings_dict.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo 'Remember to manually edit this list.'
+
 url="http://en.wikipedia.org/wiki/Wikipedia:Lists_of_common_misspellings/For_machines"
 wget -O - "$url" 2>| /dev/null \
   | sed -n '/<pre>/,/<\/pre>/p' \
