ae75e6b41ae13c7f1f104168b3658a8ae7d79248
Kevin Lyda <kevin@ie.suberic.net>
2012-04-19 21:37:05 +0100

    Clean up test runner.

--- tests/runall.py
+++ tests/runall.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 """Run ***ALL*** the testorz!!!"""
 
-import glob
 import os
 import sys
 import unittest
 
-cwd = os.path.dirname(globals().get('__file__', os.getcwd()))
+test_dir = os.path.dirname(globals().get('__file__', os.getcwd()))
 
-tests = unittest.TestLoader().discover(cwd)
+tests = unittest.TestLoader().discover(test_dir)
 runner = unittest.TextTestRunner(verbosity=2)
 result = runner.run(tests)
 if not result.wasSuccessful():
