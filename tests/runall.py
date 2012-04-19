d11a16c6ea52bb37267b327f0a857eb59abc1916
Kevin Lyda <kevin@ie.suberic.net>
2012-04-19 21:32:48 +0100

    Run tests more sensibly.
    Use the unittest suite better.  Have proper exit codes.

--- tests/runall.py
+++ tests/runall.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
 """Run ***ALL*** the testorz!!!"""
 
-import unittest
-import imp
 import glob
 import os
+import sys
+import unittest
 
 cwd = os.path.dirname(globals().get('__file__', os.getcwd()))
 
-test_list = [(os.path.basename(f)[:-3], f)
-             for f in glob.glob(os.path.join(cwd, 'test_*.py'))]
-for test_module, test_file in test_list:
-  test_runner = imp.load_source(test_module, test_file)
-  print 'Running test suit "%s"' % test_module
-  suite = unittest.defaultTestLoader.loadTestsFromModule(test_runner)
-  unittest.TextTestRunner(verbosity=2).run(suite)
+tests = unittest.TestLoader().discover(cwd)
+runner = unittest.TextTestRunner(verbosity=2)
+result = runner.run(tests)
+if not result.wasSuccessful():
+  sys.exit(1)
