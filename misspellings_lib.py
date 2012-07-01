fb8a83f79eab043914d14ba524ba112983aca955
myint <myint@macbookpro.local>
2012-07-01 08:06:42 -0700

    Drop Unicode exceptions
    Reporting them will overwhelm the user. It will report errors on all the
    "pyc" files for example.

--- misspellings_lib.py
+++ misspellings_lib.py
@@ -95,7 +95,9 @@ class Misspellings(object):
                     word.lower() in self._misspelling_dict):
                   results.append([fn, line_ct, word])
               line_ct += 1
-        except (IOError, UnicodeDecodeError):
+        except UnicodeDecodeError:
+          pass
+        except IOError:
           errors.append('%s' % sys.exc_info()[1])
     return errors, results
 
